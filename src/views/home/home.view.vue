9a6ae742c9dde09cc7213ce0e8a3829cd18c0332
Olexandr <akarakai20@gmail.com>
2024-12-30 00:05:47 +0200

    fix

--- src/views/home/home.view.vue
+++ src/views/home/home.view.vue
@@ -1,19 +1,18 @@
 <template>
-    <section class="home-content">
+    <section class="home-content grid gap-16">
         <AppDescription />
 
-        <RouterLink
-            v-for="route in routes"
-            :to="{ name: route.name }"
-            class="nav-menu-item"
-        >
-            {{ route.label }}
-        </RouterLink>
+        <div>
+            <RouterLink :to="{ name: RouteName.Lab1_1 }" class="nav-menu-item">
+                Почати
+            </RouterLink>
+        </div>
     </section>
 </template>
 
 <script lang="ts" setup>
 import AppDescription from '@/views/shared/app-description.component.vue'
+import { RouteName } from '@/router'
 </script>
 
 <style lang="scss">
