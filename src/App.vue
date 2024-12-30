65e4310202dc41f4d92182ee6aae47f3b3efaac1
Olexandr <akarakai20@gmail.com>
2024-12-30 17:05:19 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,6 @@
 <script setup lang="ts">
 import AppHeader from '@/components/navigation/app-header.component.vue'
 import AppFooter from '@/components/navigation/app-footer.component.vue'
+
+console.log()
 </script>
